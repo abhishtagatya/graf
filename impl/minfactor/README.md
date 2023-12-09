c141f312f362dc21fba79e242d532875e256d25c
abhishtagatya <abhishtagatya@yahoo.com>
2023-12-09 22:17:51 +0100

    Added MinFactor Implementation

---
+++ impl/minfactor/README.md
@@ -0,0 +1,5 @@
+# Min. Factor
+> Reduces the Graph Edges to have Minimal Required Edges
+
+The Minimal Graph implementation reduces the Graph Edges to the minimal required edges to
+construct the shortest-path for all-pairs of edges. It is the minimal factor graph by Dijkstra's algorithm.
---
+++ impl/minfactor/reduce.go
@@ -0,0 +1,53 @@
+package minfactor
+
+import (
+	"container/heap"
+	"graf"
+)
+
+// ReduceGraphEdge Reduces the Graph Edge to Minimal Amount of Edges Required
+func ReduceGraphEdge(graph graf.Graph) (*graf.Graph, error) {
+	redGraph := graf.BlankGraph()
+
+	for sid := range graph.Vertices {
+		sv := graph.Vertices[sid]
+
+		queue := graf.BlankQueue()
+		heap.Push(&queue, &graf.QueueItem{
+			Value:    sv,
+			Priority: 0,
+		})
+
+		visitMap := map[string]bool{sv.Id: false}
+		distanceMap := map[string]float64{sv.Id: 0}
+
+		for !queue.IsEmpty() {
+			cq := heap.Pop(&queue).(*graf.QueueItem)
+			cv := cq.Value.(graf.Vertex)
+
+			if visitMap[cv.Id] {
+				continue
+			}
+
+			visitMap[cv.Id] = true
+			redGraph.Vertices[cv.Id] = cv
+
+			for _, edge := range graph.Edges[cv] {
+				newDist := cq.Priority + edge.Weight
+				if dist, ok := distanceMap[edge.ConnectedId]; !ok || newDist < dist {
+					distanceMap[edge.ConnectedId] = newDist
+					heap.Push(&queue, &graf.QueueItem{
+						Value:    *edge.ConnectedVertex,
+						Priority: newDist,
+					})
+
+					if !graf.ContainsEdge(redGraph.Edges[cv], edge) {
+						redGraph.AddEdge(cv, *edge.ConnectedVertex, edge.Weight)
+					}
+				}
+			}
+		}
+	}
+
+	return &redGraph, nil
+}
