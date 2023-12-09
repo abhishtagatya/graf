748843684704f7dd7826aa846f69c08f5c6e32e3
abhishtagatya <abhishtagatya@yahoo.com>
2023-12-09 22:38:21 +0100

    Refactor Project Structure

--- graph.go
+++ graph.go
@@ -1,9 +1,5 @@
 package graf
 
-import (
-	"container/heap"
-)
-
 /* Graph Structure */
 
 type Edge struct {
@@ -63,6 +59,16 @@ func ContainsVertex(list []string, v string) bool {
 	return false
 }
 
+func ContainsEdge(edges []Edge, edge Edge) bool {
+	for _, e := range edges {
+		if e.ConnectedId == edge.ConnectedId && e.ConnectedVertex.Id == edge.ConnectedVertex.Id && e.Weight == edge.Weight {
+			return true
+		}
+	}
+
+	return false
+}
+
 func BlankGraph() Graph {
 	graph := Graph{
 		Vertices: map[string]Vertex{},
@@ -70,53 +76,4 @@ func BlankGraph() Graph {
 	}
 
 	return graph
-}
-
-/* Queue Structure */
-
-type QueueItem struct {
-	Value    interface{}
-	Priority float64
-	Index    int
-}
-
-type MinPriorityQueue []*QueueItem
-
-func (mpq MinPriorityQueue) Len() int {
-	return len(mpq)
-}
-
-func (mpq MinPriorityQueue) IsEmpty() bool {
-	return len(mpq) == 0
-}
-
-func (mpq MinPriorityQueue) Less(i, j int) bool {
-	return mpq[i].Priority < mpq[j].Priority
-}
-
-func (mpq MinPriorityQueue) Swap(i, j int) {
-	mpq[i], mpq[j] = mpq[j], mpq[i]
-	mpq[i].Index = i
-	mpq[j].Index = j
-}
-
-func (mpq *MinPriorityQueue) Push(x interface{}) {
-	item := x.(*QueueItem)
-	item.Index = len(*mpq)
-	*mpq = append(*mpq, item)
-}
-
-func (mpq *MinPriorityQueue) Pop() interface{} {
-	old := *mpq
-	n := len(old)
-	item := old[n-1]
-	item.Index = -1 // for safety
-	*mpq = old[0 : n-1]
-	return item
-}
-
-func BlankQueue() MinPriorityQueue {
-	mpq := make(MinPriorityQueue, 0)
-	heap.Init(&mpq)
-	return mpq
 }
