2026411a8ee979941ffd4ad8331a4602ed88e027
abhishtagatya <abhishtagatya@yahoo.com>
2023-12-10 20:21:51 +0100

    Added Example for Geometric Container

---
+++ example/geocontainer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"graf"
+	"graf/impl/geocontainer"
+	"os"
+	"time"
+)
+
+func main() {
+
+	if len(os.Args) <= 3 {
+		return
+	}
+
+	fileName := os.Args[1]
+	containerFile := os.Args[2]
+	source := os.Args[3]
+	target := os.Args[4]
+
+	graph, err := graf.LoadFile(fileName, "a")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	container, err := geocontainer.LoadGeoContainer(containerFile, "x")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	startTime := time.Now()
+	report, err := geocontainer.DijkstraGeometricPrune(*graph, source, target, container)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+
+	result := fmt.Sprintf("Distance\t: %f\nVisits\t: %d\nElapsed Timet\t: %s", report.Distance, len(report.VisitMap), elapsedTime)
+
+	fmt.Println("Dijkstra Geo. Prune : ")
+	fmt.Println(result)
+}
