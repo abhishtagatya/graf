939203bc5853981a68365e5bfafe171b0224bb05
abhishtagatya <abhishtagatya@yahoo.com>
2023-12-10 20:21:42 +0100

    Added Example for Dijkstra

---
+++ example/dijkstra.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"graf"
+	"graf/impl/dijkstra"
+	"os"
+	"time"
+)
+
+func main() {
+
+	if len(os.Args) <= 3 {
+		return
+	}
+
+	fileName := os.Args[1]
+	source := os.Args[2]
+	target := os.Args[3]
+
+	graph, err := graf.LoadFile(fileName, "a")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	startTime := time.Now()
+	report, err := dijkstra.SingleDijkstra(*graph, source, target)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+
+	result := fmt.Sprintf("Distance\t: %f\nVisits\t: %d\nElapsed Timet\t: %s", report.Distance, len(report.VisitMap), elapsedTime)
+
+	fmt.Println("Dijkstra : ")
+	fmt.Println(result)
+}
